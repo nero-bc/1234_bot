567a00d3fa7ebb23ea449f816e072cf2fe4d3c0b
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-08-10 17:46:15 +0530

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -1,21 +1,15 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins import merge
-from plugins import audio   # Import from merge.py
 from config import Config
 
 @Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
 async def confirm_dwnld(client, message):
-    media = message
-    filetype = media.document or media.video 
-    await message.reply_text(
-        "**What do you want me to do with this File 🤔**",
-        quote=True,
-        reply_markup=keyboard
-    )
-    # Check the MIME type
-    if filetype.mime_type.startswith("video/") or filetype.mime_type.startswith("audio/"):
-        if filetype.mime_type.startswith("video/"):
+    media = message.document or message.video or message.audio
+    filetype = media.mime_type
+    
+    if filetype.startswith("video/") or filetype.startswith("audio/"):
+        if filetype.startswith("video/"):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
                  InlineKeyboardButton(text="Remove Audio 🎧", callback_data="handle_remove_audio")],
@@ -24,9 +18,21 @@ async def confirm_dwnld(client, message):
                 [InlineKeyboardButton(text="Video+audio 📹", callback_data="set_merge_video"),
                  InlineKeyboardButton(text="CANCEL", callback_data="close")]
             ])
+        else:
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton(text="Extract Audio 📢", callback_data="download_file"),
+                 InlineKeyboardButton(text="Trim Audio ✂️", callback_data="handle_trim_audio")],
+                [InlineKeyboardButton(text="Merge Audio 🎵", callback_data="set_merge_audio"),
+                 InlineKeyboardButton(text="CANCEL", callback_data="close")]
+            ])
         
+        await message.reply_text(
+            "**What do you want me to do with this File 🤔**",
+            quote=True,
+            reply_markup=keyboard
+        )
     else:
         await message.reply_text(
             "Invalid Media",
             quote=True
-                 )
+        )
